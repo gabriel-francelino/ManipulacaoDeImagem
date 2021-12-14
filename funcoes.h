7476bbf74d819952213d9268b6e63c26a9998b64
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2021-12-14 09:25:35 -0300

    Add files via upload

---
+++ funcoes.h
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   funcoes.h
+ * Author: anaca
+ *
+ * Created on 3 de Dezembro de 2021, 13:41
+ */
+
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+int leMatriz(const char *nome);
+void escreveMatriz(const char *nome);
+void inverteMatriz(const char *nome,const char *nome1);
+void giraDireita (const char *nome,const char *nome1);
+void giraEsquerda(const char *nome,const char *nome1);
+
+#endif /* FUNCOES_H */
+
