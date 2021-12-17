7967fae4c1b7c37344246620f48db7ae1df28570
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2021-12-17 12:14:17 -0300

    Update funcoes.cc
    Girar para esquerda, espelhar verticalmente e horizontalmente funcionando

--- funcoes.cc
+++ funcoes.cc
@@ -51,11 +51,11 @@ void giraDireita(int lin, int col, int tons, int m[1000][1000]) {
             }
         }
         //escrever a matriz no arquivo
-        r = &d[0][0];
+        p1 = &d[0][0];
         for (i = 0; i < col; i++) {
             for (j = 0; j < lin; j++) {
-                myfile << *r << " ";
-                r++;
+                myfile << *p1 << " ";
+                p1++;
             }
             myfile << endl;
         }
@@ -100,15 +100,76 @@ void giraEsquerda(int lin, int col, int tons, int m[1000][1000]) {
         myfile.close();
     } else
         cout << "Não foi posssível girar a matriz!";
-
 }
 
-void inverteVertical() {
+void inverteVertical(int lin, int col, int tons, int m[1000][1000]) {
+    int *p, *p1, *q, *r;
+    int d[col][lin];
+    int tam = lin*col;
+    ofstream myfile;
+    myfile.open("teste3.pgm");
+    if (myfile.is_open()) {
+        myfile << "P2" << endl;
+        myfile << col << " " << lin << endl;
+        myfile << tons << endl;
 
+        //espelhar verticalmente
+        p1 = &d[0][0];
+        for ( p = &m[0][0]+(tam-col); p>=&m[0][0]; ) {
+            for ( q=p; q < p+col ; q++ ) {
+                *p1 = *q; 
+                p1++;
+            } 
+            p-=col;
+        }
+        
+        //escrever a matriz no arquivo
+        r = &d[0][0];
+        for (i = 0; i < lin; i++) {
+            for (j = 0; j < col; j++) {
+                myfile << *r << " ";
+                r++;
+            }
+            myfile << endl;
+        }
+        myfile.close();
+    } else
+        cout << "Não foi posssível girar a matriz!";
 }
 
-void inverteHorizontal() {
+void inverteHorizontal(int lin, int col, int tons, int m[1000][1000]) {
+    int *p, *p1, *q, *r;
+    int d[col][lin];
+    int tam = lin*col;
+    ofstream myfile;
+    myfile.open("teste3.pgm");
+    if (myfile.is_open()) {
+        myfile << "P2" << endl;
+        myfile << col << " " << lin << endl;
+        myfile << tons << endl;
 
+        //espelhar horizontalmente
+        p1 = &d[0][0];
+        for ( p = &m[0][0]+(col-1); p<&m[0][0]+tam; ) {
+            for ( q=p; q > p-col ; q-- ) {
+                *p1 = *q; 
+                p1++;
+            } 
+            p+=col;
+        }
+        
+        //escrever a matriz no arquivo
+        r = &d[0][0];
+        for (i = 0; i < lin; i++) {
+            for (j = 0; j < col; j++) {
+                myfile << *r << " ";
+                r++;
+            }
+            myfile << endl;
+        }
+        myfile.close();
+    } else
+        cout << "Não foi posssível girar a matriz!";
 }
 
 void escureceBordas(){
